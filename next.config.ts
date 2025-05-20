9301effcdfd817194c9bfef4175a5798667889d4
talal-mahmood <talal.mehmood@byteb.io>
2025-05-20 17:50:20 +0500

    fix: revert next config changes

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from 'next';
 
-const nextConfig = {
-  basePath: '/portfolio',
-  assetPrefix: '/portfolio',
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
